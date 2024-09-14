f6ac244dd7a1543b96e97241dac8315039fa4779
Kevin Carvalho.com <kverdolincarvalho@gmail.com>
2024-09-14 11:23:39 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Digite a palavra a ser usada");
+string palavra = Console.ReadLine();
+int contador = 0;
+
+foreach (var letra in palavra)
+{
+    if(letra.Equals('a') || letra.Equals('A'))
+        contador++;
+
+}
+Console.WriteLine($"A palavra tem: {contador} letras 'A'");
